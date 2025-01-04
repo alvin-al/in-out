87a52b8e91b331b05930274c3acf86026c5ca31a
Alvin Al <alvvxal@gmail.com>
2025-01-04 17:00:24 +0700

    Change middleware

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,13 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   const token = request.cookies.get("user_token");
 
+  //Redirect jika sudah login
+  if (request.nextUrl.pathname === "/") {
+    if (token && token.value !== "") {
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
+  }
+
   // Redirect ke /login jika token tidak ada atau kosong
   if (!token || token.value === "") {
     return NextResponse.redirect(new URL("/login", request.url));
