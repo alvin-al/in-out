25fc09cd96ed3014ee1e6433a85119a51a5a48c9
Alvin Al <alvvxal@gmail.com>
2025-01-06 14:42:48 +0700

    Change camera

--- components/elements/DashboardCard.tsx
+++ components/elements/DashboardCard.tsx
@@ -6,9 +6,9 @@ const DashboardCard = ({ icon, title, value, uom, className }: CardProps) => {
   return (
     <Link href='/crate-detail'>
       <div
-        className={`${className} w-full h-fit bg-gray-300 rounded-xl flex p-8 flex-wrap gap-5`}
+        className={`${className} w-full h-fit bg-gray-300 rounded-xl flex p-8 flex-wrap gap-4`}
       >
-        <div className='flex text-gray-500 gap-4'>
+        <div className='flex text-gray-500 gap-2'>
           <div className=''>{icon}</div>
           <div className='text-lg font-medium'>{title}</div>
         </div>
